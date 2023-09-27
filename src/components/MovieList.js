cb8a11d8566ea28f8c09f5fe3d406853d339d21b
Aniruddh <anirudhbhadauriya@gmail.com>
2023-09-27 18:06:35 +0530

    removed the new bug of all horizontal scrolling

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,12 +1,12 @@
 import React from "react";
 import MovieCard from "./MovieCard";
+import "./MovieList.css"; // Import your custom CSS file
 
 const MovieList = ({ title, movies }) => {
-  // console.log(movies);
   return (
-    <div className="px-6 bg-black">
+    <div className="-ml-8 px-6 bg-black">
       <h1 className="text-3xl py-4 md:text-3xl py-4 text-white">{title}</h1>
-      <div className="flex overflow-x-scroll">
+      <div className="flex overflow-x-scroll custom-scrollbar">
         <div className="flex">
           {movies?.map((movie) => (
             <MovieCard key={movie.id} poster_path={movie.poster_path} />
